0a09844d888857c409069960c1ea6233511018df
Dave Schukin <daveschukin@gmail.com>
2018-12-18 19:53:16 -0500

    First Dave commit

--- app/app.js
+++ app/app.js
@@ -6,18 +6,9 @@ var $$ = document.querySelectorAll.bind(document);
 var App = function($el){
   this.$el = $el;
   this.load();
-
-  this.$el.addEventListener(
-    'submit', this.submit.bind(this)
-  );
-
   this.renderAgeLoop();
 
-  // if (this.dob) {
-  //   this.renderAgeLoop();
-  // } else {
-  //   this.renderChoose();
-  // }
+  console.log("Test 1");
 };
 
 App.fn = App.prototype;
@@ -25,33 +16,47 @@ App.fn = App.prototype;
 App.fn.load = function(){
   var value;
 
-  this.dob = new Date("July 20, 1995 02:00:00");
+  this.dob = null;
 
-  // if (value = localStorage.dob) {
-  //   this.dob = new Date(parseInt(value));
-  //   this.dob.setTime(this.dob.getTime() + 1000 * 60 * this.dob.getTimezoneOffset());
-  // }
+  if (value = localStorage.dob) {
+    this.dob = new Date(parseInt(value));
+  }
+
+  var lastUpdated;
+
+  if (lastUpdated = localStorage.lastUpdated) {
+    this.lastUpdated = new Date(parseInt(lastUpdated));
+  }
+
+  this.fetchHorizon();
 };
 
 App.fn.save = function(){
-  if (this.dob)
+  if (this.dob) {
     localStorage.dob = this.dob.getTime();
+    localStorage.lastUpdated = (new Date()).getTime();
+  }
 };
 
-App.fn.submit = function(e){
-  e.preventDefault();
-
-  var input = this.$$('input')[0];
-  if ( !input.valueAsDate ) return;
-
-  this.dob = input.valueAsDate;
-  this.save();
-  this.dob.setTime(this.dob.getTime() + 1000 * 60 * this.dob.getTimezoneOffset());
-  this.renderAgeLoop();
-};
-
-App.fn.renderChoose = function(){
-  this.html(this.view('dob')());
+App.fn.fetchHorizon = function() {
+  var app = this;
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "https://obsr-plex-test.herokuapp.com/horizon.json", true);
+  xhr.setRequestHeader("Content-Type", "*/*");
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4) {
+      // JSON.parse does not evaluate the attacker's scripts.
+      var resp = JSON.parse(xhr.responseText);
+      var timestampString = resp.timestamp;
+      var timestamp = new Date(timestampString);
+      app.dob = timestamp;
+      app.save();
+      app.lastUpdatedString = '';
+    } else {
+      app.lastUpdatedString = app.newLastUpdatedString();
+    }
+  }
+  xhr.send();
 };
 
 App.fn.renderAgeLoop = function(){
@@ -83,12 +88,65 @@ App.fn.calculateMS = function(){
 };
 
 App.fn.renderAge = function(){
+  var now       = new Date();
+  var duration  = this.dob - now;
+  var years     = duration / 31556900000;
+  var days      = duration / 86400000;
+
+  var majorMinor = days.toFixed(9).toString().split('.');
+  var app = this;
+  var lastUpdatedString = this.lastUpdatedString;
+
   requestAnimationFrame(function(){
     this.html(this.view('age')({
-      year:         this.calculateAge(),
-      milliseconds: this.calculateMS()
+      year:         majorMinor[0],
+      milliseconds: majorMinor[1],
+      noticeText:   lastUpdatedString
     }));
   }.bind(this));
+};
+
+App.fn.newLastUpdatedString = function() {
+  if (this.lastUpdated == undefined || this.lastUpdated == null) {
+    return "Last updated: never"
+  }
+
+  var now = new Date();
+  var durationSinceLastUpdated = (now - this.lastUpdated) / 1000;
+
+  durationSinceLastUpdated = 60 * 60 * 25 * 3;
+
+  if (durationSinceLastUpdated > 5) {
+    var timeAgoInWords = this.timeAgoInWords(durationSinceLastUpdated);
+    var result = "Last updated: " + timeAgoInWords + " ago";
+    
+    if (durationSinceLastUpdated > (60 * 60 * 24)) {
+      result = "WARNING: We haven't been able to contact server to get latest end time in " + timeAgoInWords + "."
+    }
+
+    return result;
+  } else {
+    return "";
+  }
+};
+
+App.fn.timeAgoInWords = function(seconds) {
+  if (seconds < 60) {
+    return "" + Math.round(seconds) + " seconds";
+  }
+
+  var minutes = seconds / 60;
+  if (minutes < 60) {
+    return "" + Math.round(minutes) + " minutes";
+  }
+
+  var hours = minutes / 60;
+  if (hours < 24) {
+    return "" + Math.round(hours) + " hours";
+  }
+
+  var days = hours / 24;
+  return "" + Math.round(days) + " days";
 };
 
 App.fn.$$ = function(sel){
